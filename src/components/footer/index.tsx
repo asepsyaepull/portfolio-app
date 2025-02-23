a7838ec5ed3038fdc8cd7bad084b32cb8343cdca
asepsyaepull <asep.syaepul95@gmail.com>
2025-02-23 20:51:46 +0700

    Add footer component with portfolio information and copyright notice

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="mt-12 bg-zinc-900 border border-slate-700 rounded-lg p-4 flex justify-between items-center">
+            <div className="text-white">
+                <h3 className="font-medium">Asep Syaepul —</h3>
+                <p className="text-sm text-gray-400">Portfolio</p>
+            </div>
+            <p className="text-gray-400 text-sm">© 2025 Asys_ All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
