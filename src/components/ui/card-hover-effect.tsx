63f94e47866c227ec653b3d43c0efa5b8f8a87fb
asepsyaepull <asep.syaepul95@gmail.com>
2025-02-26 21:44:07 +0700

    Update card component styles for improved light and dark mode visibility

--- src/components/ui/card-hover-effect.tsx
+++ src/components/ui/card-hover-effect.tsx
@@ -66,7 +66,7 @@ export const Card = ({
   return (
     <div
       className={cn(
-        "rounded-2xl h-full w-full p-4 overflow-hidden bg-zinc-900 border border-slate-700 dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
+        "rounded-2xl h-full w-full p-4 overflow-hidden bg-white border dark:bg-zinc-900  border-gray-300 dark:border-slate-700 group-hover:border-slate-700 relative z-20",
         className
       )}
     >
@@ -85,7 +85,7 @@ export const CardTitle = ({
   children: React.ReactNode;
 }) => {
   return (
-    <h4 className={cn("text-zinc-100 font-bold tracking-wide mt-4", className)}>
+    <h4 className={cn("text-gray-900 dark:text-zinc-100 font-bold tracking-wide mt-4", className)}>
       {children}
     </h4>
   );
@@ -101,7 +101,7 @@ export const CardDescription = ({
   return (
     <p
       className={cn(
-        "mt-8 text-zinc-400 tracking-wide leading-relaxed text-sm",
+        "mt-8 text-gray-600 dark:text-zinc-400 tracking-wide leading-relaxed text-sm",
         className
       )}
     >
